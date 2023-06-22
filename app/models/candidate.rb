2bdf270038978cd1ab4db58848f6f9470c4e7b4f
Pawel Maryon <pawelmaryon@pawels-air.lan>
2023-06-22 10:35:35 +0100

    validations removed - causing issues uploading the file

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,4 @@
 class Candidate < ApplicationRecord
   belongs_to :campaign
   has_many :votes
-  validates :name, uniqueness: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,9 +2,4 @@ class Vote < ApplicationRecord
   belongs_to :candidate
   belongs_to :campaign
   validates :validity, presence: true
-  validates :choice, presence: true
-  validates :conn, presence: true
-  validates :msisdn, presence: true, format: { with: /\A\d{10}\z/, message: "must be a 10-digit number" }
-  validates :guid, presence: true
-  validates :shortcode, presence: true
 end
